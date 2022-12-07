d15e48753756cffb23f845167e9761cf3fa2b372
Boonyarith Piriyothinkul <boonyarith.pir@gmail.com>
2022-12-07 21:23:45 +0700

    add start/stop/set_saymyname_template commands

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,63 @@
 import * as dotenv from "dotenv" // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config() //Must be invoked before all statements
 
-import { Client, Intents } from "discord.js"
+import {
+	Client,
+	Interaction,
+	IntentsBitField,
+	SlashCommandBuilder,
+	Collection,
+	CommandInteraction,
+	ChatInputCommandInteraction,
+} from "discord.js"
 import { joinVoiceChannel, createAudioPlayer, createAudioResource } from "@discordjs/voice"
 import discordTTS from "discord-tts"
 
+let enabledSayMyName = true
+let sayMyNameTemplate = "{name} เข้ามาจ้า"
 const BOT_ID = "947897258014298162"
 let connection
 
+export const commandsConfig = {
+	stop: {
+		data: new SlashCommandBuilder().setName("stop").setDescription("Stops SAY MY NAME!"),
+		async execute(interaction) {
+			enabledSayMyName = false
+			await interaction.reply({ content: "Say your name is disabled!" })
+		},
+	},
+	start: {
+		data: new SlashCommandBuilder().setName("start").setDescription("Starts SAY MY NAME!"),
+		async execute(interaction) {
+			enabledSayMyName = true
+			await interaction.reply({ content: "Say your name is enabled!" })
+		},
+	},
+	set_saymyname_template: {
+		data: new SlashCommandBuilder()
+			.setName("set_saymyname_template")
+			.setDescription("Sets how bot should greeting an user")
+			.addStringOption((option) =>
+				option
+					.setName("template")
+					.setDescription(`Template of the tts. use {name} to positioning the displayname. Example: "{name} เข้ามาจ้า"`)
+					.setRequired(true)
+			),
+		async execute(interaction: ChatInputCommandInteraction) {
+			const unsanitizedTemplate = interaction.options.getString("template")
+			if (!unsanitizedTemplate?.includes("{name}")) {
+				await interaction.reply({ content: "Template is wrong. Could not find {name} in the teamplate.", ephemeral: true })
+				return
+			}
+
+			sayMyNameTemplate = unsanitizedTemplate
+			await interaction.reply({ content: `Successfully updates a template! New teamplate is: ${sayMyNameTemplate}` })
+		},
+	},
+}
+
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES],
+	intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.GuildVoiceStates],
 })
 
 client.login(process.env.TOKEN)
@@ -20,6 +68,7 @@ client.on("ready", () => {
 
 client.on("voiceStateUpdate", async (prevState, newState) => {
 	if (newState.member.id === BOT_ID) return
+	if (!enabledSayMyName) return
 
 	if (newState.channel?.id && prevState.channel?.id !== newState.channel?.id) {
 		if (prevState.channel?.id !== newState.channel.id) {
@@ -32,10 +81,9 @@ client.on("voiceStateUpdate", async (prevState, newState) => {
 			})
 		}
 
+		const text = sayMyNameTemplate.replace("{name}", newState.member.displayName)
 		const audioPlayer = createAudioPlayer()
-		const resource = createAudioResource(
-			discordTTS.getVoiceStream(`${newState.member.displayName} เข้ามาจ้า`, { lang: "th" })
-		)
+		const resource = createAudioResource(discordTTS.getVoiceStream(text, { lang: "th" }))
 		audioPlayer.play(resource)
 		const subscription = connection.subscribe(audioPlayer)
 
@@ -43,4 +91,17 @@ client.on("voiceStateUpdate", async (prevState, newState) => {
 			setTimeout(() => subscription.unsubscribe(), 10_000)
 		}
 	}
+})
+
+client.on("interactionCreate", async (interaction: Interaction) => {
+	if (!interaction.isCommand()) return
+
+	if (!Object.keys(commandsConfig).includes(interaction.commandName)) {
+		await interaction.reply({
+			content: `Command not found: ${interaction.commandName} isn't in the config key.`,
+			ephemeral: true,
+		})
+	}
+
+	commandsConfig[interaction.commandName].execute(interaction)
 })
---
+++ src/scripts/registerCommands.ts
@@ -0,0 +1,27 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+import { commandsConfig } from '../index'
+
+const BOT_ID = "947897258014298162"
+const commands = Object.values(commandsConfig).map(item => item.data.toJSON())
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+// and deploy your commands!
+(async () => {
+	try {
+		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+		// The put method is used to fully refresh all commands in the guild with the current set
+		const data = await rest.put(
+			Routes.applicationCommands(BOT_ID),
+			{ body: commands },
+		);
+
+		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+	} catch (error) {
+		// And of course, make sure you catch and log any errors!
+		console.error(error);
+	}
+})();
