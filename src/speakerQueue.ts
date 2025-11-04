3d3122c302cce4da8a1e84a1c9d8c692b55ce9ec
Boonyarith P <boonyarith.pir@gmail.com>
2025-11-05 00:07:25 +0700

    add error log

--- src/speakerQueue.ts
+++ src/speakerQueue.ts
@@ -78,74 +78,79 @@ const getSameUserSameChannelQueue = (memberId: string, channelId: string): Queue
 }
 
 const consumeQueue = async () => {
-	setQueueState(QueueState.PROCESSING)
-	for (let queueLength = 0; queueLength !== queue.length; queueLength = queue.length) {
-		await new Promise((resolve) => setTimeout(resolve, 1000))
-	}
-
-	logger.info('queue before consuming', JSON.stringify(queue))
-	while (queue.length > 0) {
-		const firstEvent = queue[0]
-
-		const samePeopleSameChannelEvents = getSameUserSameChannelQueue(
-			firstEvent.payload.memberId,
-			firstEvent.payload.channelId
-		)
-		if (samePeopleSameChannelEvents.length > 1) {
-			const samePeopleSameChannelEventIds = samePeopleSameChannelEvents.map((event) => event.id)
-			queue = queue.filter((item) => !samePeopleSameChannelEventIds.includes(item.id))
-			logger.info('consume queue', JSON.stringify(samePeopleSameChannelEvents))
-
-			const name =
-				getAllAlias[samePeopleSameChannelEvents[0].payload.memberId] ||
-				samePeopleSameChannelEvents[0].payload.displayName
-			const text = `เข้าออกทำเหี้ยอะไร ${name}`
-
-			await joinChannelAndSpeak(
-				samePeopleSameChannelEvents[0].payload.guildId,
-				samePeopleSameChannelEvents[0].payload.channelId,
-				samePeopleSameChannelEvents[0].payload.adapterCreator,
-				text
-			)
-
-			continue
+	try {
+		setQueueState(QueueState.PROCESSING)
+		for (let queueLength = 0; queueLength !== queue.length; queueLength = queue.length) {
+			await new Promise((resolve) => setTimeout(resolve, 1000))
 		}
 
-		const selectedEvents = getSameTypeSameChannelQueue(firstEvent.type, firstEvent.payload.channelId)
-		const selectedEventsId = selectedEvents.map((event) => event.id)
-		queue = queue.filter((item) => !selectedEventsId.includes(item.id))
-		logger.info('consume queue', JSON.stringify(selectedEvents))
+		logger.info('queue before consuming', JSON.stringify(queue))
+		while (queue.length > 0) {
+			const firstEvent = queue[0]
 
-		if (selectedEvents.length > 1) {
-			const names = selectedEvents.map((event) => {
+			const samePeopleSameChannelEvents = getSameUserSameChannelQueue(
+				firstEvent.payload.memberId,
+				firstEvent.payload.channelId
+			)
+			if (samePeopleSameChannelEvents.length > 1) {
+				const samePeopleSameChannelEventIds = samePeopleSameChannelEvents.map((event) => event.id)
+				queue = queue.filter((item) => !samePeopleSameChannelEventIds.includes(item.id))
+				logger.info('consume queue', JSON.stringify(samePeopleSameChannelEvents))
+
+				const name =
+					getAllAlias[samePeopleSameChannelEvents[0].payload.memberId] ||
+					samePeopleSameChannelEvents[0].payload.displayName
+				const text = `เข้าออกทำเหี้ยอะไร ${name}`
+
+				await joinChannelAndSpeak(
+					samePeopleSameChannelEvents[0].payload.guildId,
+					samePeopleSameChannelEvents[0].payload.channelId,
+					samePeopleSameChannelEvents[0].payload.adapterCreator,
+					text
+				)
+
+				continue
+			}
+
+			const selectedEvents = getSameTypeSameChannelQueue(firstEvent.type, firstEvent.payload.channelId)
+			const selectedEventsId = selectedEvents.map((event) => event.id)
+			queue = queue.filter((item) => !selectedEventsId.includes(item.id))
+			logger.info('consume queue', JSON.stringify(selectedEvents))
+
+			if (selectedEvents.length > 1) {
+				const names = selectedEvents.map((event) => {
+					const alias = getAllAlias()
+					return alias[event.payload.memberId] || event.payload.displayName
+				})
+				const text = getTextSpeechForMultipleMember(names, selectedEvents[0].type)
+
+				await joinChannelAndSpeak(
+					selectedEvents[0].payload.guildId,
+					selectedEvents[0].payload.channelId,
+					selectedEvents[0].payload.adapterCreator,
+					text
+				)
+
+				continue
+			}
+
+			if (selectedEvents.length === 1) {
+				const { payload, type } = selectedEvents[0]
+
 				const alias = getAllAlias()
-				return alias[event.payload.memberId] || event.payload.displayName
-			})
-			const text = getTextSpeechForMultipleMember(names, selectedEvents[0].type)
+				const name = alias[payload.memberId] || payload.displayName
+				const text = getTextSpeechForSingleMember(name, type)
 
-			await joinChannelAndSpeak(
-				selectedEvents[0].payload.guildId,
-				selectedEvents[0].payload.channelId,
-				selectedEvents[0].payload.adapterCreator,
-				text
-			)
+				await joinChannelAndSpeak(payload.guildId, payload.channelId, payload.adapterCreator, text)
 
-			continue
+				continue
+			}
 		}
 
-		if (selectedEvents.length === 1) {
-			const { payload, type } = selectedEvents[0]
-
-			const alias = getAllAlias()
-			const name = alias[payload.memberId] || payload.displayName
-			const text = getTextSpeechForSingleMember(name, type)
-
-			await joinChannelAndSpeak(payload.guildId, payload.channelId, payload.adapterCreator, text)
-
-			continue
-		}
+		logger.info('queue after consuming', JSON.stringify(queue))
+	} catch (error) {
+		logger.error('consumeQueue()', 'Error occurred while consuming queue:', error)
+	} finally {
+		setQueueState(QueueState.IDLE)
 	}
-
-	logger.info('queue after consuming', JSON.stringify(queue))
-	setQueueState(QueueState.IDLE)
 }
